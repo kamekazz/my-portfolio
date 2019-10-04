79eeb359eac031d20523c3d032c58e5d996b512b
kamekazz <taveras78@gmail.com>
2019-10-03 22:47:35 -0400

    done

--- client/src/components/About/Banner.jsx
+++ client/src/components/About/Banner.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
- 
+
 class Banner extends React.Component {
-    render(){
-        return (
-            <section className="page-title-area uk-page-title">
-                <div className="uk-container">
-                    <h1>About</h1>
-                    <ul>
-                        <li><a href="/home-four">Home</a></li>
-                        <li>About</li>
-                    </ul>
-                </div>
-            </section>
-        );
-    }
+  render() {
+    return (
+      <section className='page-title-area uk-page-title'>
+        <div className='uk-container'>
+          <h1>About</h1>
+          <ul>
+            <li>
+              <a href='/'>Home</a>
+            </li>
+            <li>About</li>
+          </ul>
+        </div>
+      </section>
+    );
+  }
 }
- 
+
 export default Banner;
