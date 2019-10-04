18cdd48ac23e29ede902a116dea432ad90ef471d
kamekazz <taveras78@gmail.com>
2019-10-03 20:59:53 -0400

    tem color

--- client/src/components/Pages/Contact.jsx
+++ client/src/components/Pages/Contact.jsx
@@ -3,18 +3,18 @@ import NavigationFour from '../Navigation/NavigationFour';
 import Footer from '../Common/Footer';
 import Banner from '../Contact/Banner';
 import Contact from '../HomeTwo/Contact';
- 
+
 class ContactUs extends React.Component {
-    render(){
-        return (
-            <React.Fragment>
-                <NavigationFour />
-                <Banner />
-                <Contact />
-                <Footer />
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <NavigationFour />
+        <Banner />
+        <Contact />
+        <Footer />
+      </React.Fragment>
+    );
+  }
 }
- 
+
 export default ContactUs;
